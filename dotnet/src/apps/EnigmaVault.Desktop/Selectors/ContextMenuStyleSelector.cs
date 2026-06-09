708aefe0336d804fe8787a2b4b6819f464ec7b35
Light Play <lightplay456@gmail.com>
2026-06-09 17:06:55 +0300

    feat: Селектор стилей для контекстного меню

---
+++ dotnet/src/apps/EnigmaVault.Desktop/Selectors/ContextMenuStyleSelector.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EnigmaVault.Desktop.Selectors
+{
+    public class ContextMenuStyleSelector : StyleSelector
+    {
+        public Style? MenuItemStyle { get; set; }
+
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            if (item is MenuItem)
+                return MenuItemStyle!;
+
+            return base.SelectStyle(item, container);
+        }
+    }
+}
