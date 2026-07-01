7827c664c56dd59768e08eed5f7887f96a2f44d9
Light Play <lightplay456@gmail.com>
2026-07-01 15:23:40 +0300

    feat: реализован дизайн страницы дашборда

---
+++ web/enigma-vault-ui/src/features/overview/page/overview.page.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'overview-page',
+    templateUrl: './overview.page.html',
+    styleUrls: ['./overview.page.scss'],
+    standalone: true
+})
+export class OverviewPage {
+    
+}
