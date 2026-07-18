9bcc662dfc4563dc38489642a8e4703f8da8e1b3
Light Play <lightplay456@gmail.com>
2026-07-18 17:20:48 +0300

    feat: реализована группировка иконок по категориям

--- web/enigma-vault-ui/src/shared/ui/icons/icons.component.ts
+++ web/enigma-vault-ui/src/shared/ui/icons/icons.component.ts
@@ -1,6 +1,8 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { AssetUrlResponse } from '../../../features/passwords/models/dto/asset-urls.response';
-import { CdkContextMenuTrigger, CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
+import { CdkContextMenuTrigger, CdkMenu, CdkMenuItem } from '@angular/cdk/menu';
+import { IconCategoryResponse } from '../../../features/passwords/models/dto/icon-category.response';
+import { TooltipDirective } from '../../directives/tooltip.directive';
 
 @Component({
   selector: 'icons',
@@ -8,6 +10,7 @@ import { CdkContextMenuTrigger, CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@an
   styleUrl: './icons.component.scss',
   standalone: true,
   imports: [
+    TooltipDirective,
     CdkMenu,
     CdkMenuItem,
     CdkContextMenuTrigger
@@ -15,5 +18,34 @@ import { CdkContextMenuTrigger, CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@an
 })
 export class IconsComponent {
   icons = input.required<AssetUrlResponse[]>();
+  categories = input.required<IconCategoryResponse[]>();
+  groupedIcons = computed(() =>{
+    const icons = this.icons();
+    const categories = this.categories();
+
+    const categoryMap = new Map<string, string>();
+    for (const cat of categories) {
+      categoryMap.set(cat.categoryId, cat.name);
+    }
+
+    const groups = new Map<string, AssetUrlResponse[]>();
+    for (const icon of icons){
+      const categoryName = categoryMap.get(icon.categoryId) || 'Без категории';
+
+      if (!groups.has(categoryName)) 
+        groups.set(categoryName, []);
+
+      groups.get(categoryName)!.push(icon);
+    }
+
+    return Array.from(groups.entries())
+      .sort(([titleA], [titleB]) => titleA.localeCompare(titleB, 'ru'))
+      .map(([title, groupIcons]) => {
+        const sortedIcons = [...groupIcons].sort((a, b) => a.assetName.localeCompare(b.assetName, 'ru'));
+
+        return { title, icons: sortedIcons };
+      })
+  });
+
   selectedIcon = output<AssetUrlResponse>();
 }
